a77ff5eebcee041f119f2e1568cd556ec39311c5
lokeshpeethani2 <lokeshpeethani2@gmail.com>
2022-02-23 10:37:52 +0000

    styling

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  image:string="https://coloringhome.com/coloring/9ip/b77/9ipb77d5T.gif"
 
   ngOnInit(): void {
   }
